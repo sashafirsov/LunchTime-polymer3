b050457d4b831a78c772b4bb01fb6d37e79c3d15
sashafirsov <suns@firsov.net>
2018-09-14 21:12:50 -0700

    * sanitized unused code in lt-coffee.js

--- src/lt-coffee.js
+++ src/lt-coffee.js
@@ -13,72 +13,57 @@ import './shared-styles.js';
 class LtCoffee extends PolymerElement {
     static get template() {
         return html`
-        <style include="shared-styles">
-            :host {
-              display: block;
-              padding: 10px;
-            }
-            img{ max-width: 10em;max-height: 10em; }
-            .card img{ float: right }
-            label{ display: inline-block }
-        </style>
-
-        <div class="card" style="text-align: center">
-            <h1>Lets get coffee with someone random </h1>
-            <div>
-                <paper-radio-button>
-                    <div> [[seeker.nickname]]</div>
-                    <img src="[[seeker.imageUrl]]"/>
-                </paper-radio-button>
-                <paper-radio-button>
-                    <div></div>
-                    <img src="http://www.stickpng.com/assets/images/5a01907e7ca233f48ba62720.png"/>
-                </paper-radio-button>
-                <paper-radio-button>
-                    <div> [[selected.nickname]]</div>
-                    <img src="[[selected.imageUrl]]"/>
-                </paper-radio-button>
+            <style include="shared-styles">
+                :host {
+                    display: block;
+                    padding: 10px;
+                }
+                img{ max-width: 10em;max-height: 10em; }
+                .card img{ float: right }
+                label{ display: inline-block }
+            </style>
+    
+            <div class="card" style="text-align: center">
+                <h1>Lets get coffee with someone random </h1>
+                <div>
+                    <paper-radio-button>
+                        <div> [[seeker.nickname]]</div>
+                        <img src="[[seeker.imageUrl]]"/>
+                    </paper-radio-button>
+                    <paper-radio-button>
+                        <div></div>
+                        <img src="http://www.stickpng.com/assets/images/5a01907e7ca233f48ba62720.png"/>
+                    </paper-radio-button>
+                    <paper-radio-button>
+                        <div> [[selected.nickname]]</div>
+                        <img src="[[selected.imageUrl]]"/>
+                    </paper-radio-button>
+                </div>
+            
+                <paper-button raised on-click="_save"> Lets go!</paper-button>
             </div>
-        
-            <paper-button raised on-click="_save"> Lets go!</paper-button>
-        </div>
-        <lt-team-list selected="{{selected}}" team="[[team]]" ></lt-team-list>        
-    `;
+            <lt-team-list selected="{{selected}}" team="[[team]]" ></lt-team-list>        
+        `;
     }
 
     static get properties() {
         return {
             seeker: Object,
-            selectedId: {type: String, observer: '_idChanged', notify: true},
-            selected: {type: Object, notify: true},
-            team: {type: Array},
+            selected: Object,
+            team: Array,
         };
     }
 
     ready() {
         if (!this.selected)
             this.selected = this.team[this.team.length - 1];
-        this.nickname = this.selected.nickname;
-        this.imageUrl = this.selected.imageUrl;
 
         super.ready();
     }
 
-    _idChanged(id) {
-        const o = this.team.find(el => el.id === id);
-        Object.assign(this, o);
-        this.set('selected', o);
-    }
 
-    _fixPersonal() {
-        if (!this.nickname)
-            this.nickname = "Anonymous";
-        if (!this.imageUrl)
-            this.imageUrl = "http://www.stickpng.com/assets/images/5874cfb542e4d628738559e5.png";
-    }
 
     _save() {
-        this._fixPersonal();
         let i = this.team.indexOf(this.selected);
         this.set(`selected.nickname`, this.nickname);
         this.set(`selected.imageUrl`, this.imageUrl);
@@ -86,14 +71,6 @@ class LtCoffee extends PolymerElement {
         this.notifyPath(`team.${i}.imageUrl`, this.imageUrl);
     }
 
-    _create() {
-        this._fixPersonal();
-
-        const o = {id: this.team.length, nickname: this.nickname, imageUrl: this.imageUrl};
-        this.unshift('team', o);
-        this.selected = o;
-        this.selectedId = o.id;
-    }
 }
 
 window.customElements.define('lt-coffee', LtCoffee);
